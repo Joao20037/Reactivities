e0a534933c1c97337e9ab4dcee03dea2bc23210f
Treinamento <Treinamento@IGN004.PSSOLUCOES.local>
2025-09-05 10:20:44 -0300

    End of section 9

--- client/src/lib/types/hooks/useActivities.ts
+++ client/src/lib/types/hooks/useActivities.ts
@@ -1,15 +1,18 @@
 import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
 import agent from "../../api/agents";
+import { useLocation } from "react-router";
 
 export const useActivities = (id?: string) => {
-    const queryClient = useQueryClient()
+    const queryClient = useQueryClient();
+    const location = useLocation();
     
     const {data: activities, isPending} = useQuery({
         queryKey: ['activities'],
         queryFn: async () => {
           const response = await agent.get<Activity[]>("/activities");
           return response.data;
-        }
+        },
+        enabled: !id && location.pathname === "/activities"
       })
 
     // isLoading and isPending is not that different just exploring different things 
